74c5d96e6b854ef66832761cda9a9d6f1968f0a1
XuzhengChang <changxuzheng@huawei.com>
2022-05-20 11:04:19 +0800

    set InsecureSkipVerify=false

--- h2conn-client.go
+++ h2conn-client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"io"
 	"log"
@@ -10,21 +11,32 @@ import (
 	"time"
 
 	"github.com/posener/h2conn"
+	"golang.org/x/net/http2"
+)
+
+const (
+	serverURL1 = "https://http2.golang.org/ECHO"
+	serverURL2 = "https://localhost:8000"
 )
 
 // A client for Go's HTTP2 echo server example at http2.golang.org/ECHO
 
 func main() {
 	// Create a client, that uses the HTTP PUT method.
-	c := h2conn.Client{Method: http.MethodPut}
+	c := h2conn.Client{
+		Method: http.MethodGet,
+		Client: &http.Client{Transport: &http2.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}},
+	}
 
 	// Connect to the HTTP2 server
 	// The returned conn can be used to:
 	//   1. Write - send data to the server.
 	//   2. Read - receive data from the server.
-	conn, resp, err := c.Connect(context.Background(), "https://http2.golang.org/ECHO")
+	conn, resp, err := c.Connect(context.Background(), serverURL1)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err, "xxx")
 	}
 	defer conn.Close()
 	log.Printf("Got: %d", resp.StatusCode)
